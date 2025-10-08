9474000002b17eb6f17dae891cfb5bcc293d3e3f
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-08 20:31:59 +0530

    ShowCase card

--- src/Components/ShowCase.tsx
+++ src/Components/ShowCase.tsx
@@ -1,40 +1,40 @@
 import { Trophy } from "lucide-react"
 export const ShowCase = () => {
     return (
-        <div className="w-[25%] bg-[#ff6467] h-full">
-            <div className="grid grid-rows-3 p-6 gap-5">
-                <div className="grid grid-cols-3 rounded">
-                    <div className="col-span-1 bg-[#fafafa] h-12 w-14 flex justify-center items-center rounded">
+        <div className="w-[20%] bg-[#ff6467] h-full">
+            <div className="grid grid-rows-3 p-6 gap-4">
+
+
+
+                <div className="grid grid-cols-3 gap-6 rounded-lg">
+                    <div className="btn-shadow col-span-1 bg-[#fafafa] flex justify-center items-center border-2 rounded">
                         <Trophy size={28} />
                     </div>
 
-                    <div className="grid col-span-2 bg-[#fafafa] rounded justify-center items-center">
-                        3X Hackathons Wins
+                    <div className="grid col-span-2 bg-[#fafafa] justify-center items-center border-2 rounded btn-shadow">
+                        <span className="font-bold p-2 text-[15px]">3X Hackathons Wins</span>
                     </div>
                 </div>
 
-
-                <div className=" grid grid-cols-3 gap-6 rounded">
-                    <div className="grid col-span-2 gap-12 bg-[#fafafa] rounded justify-center items-center">
-                        <span>3X Hackathons Wins</span>
+                <div className="grid grid-cols-3 gap-6 rounded">
+                    <div className="grid col-span-2 bg-[#fafafa] h-12 justify-center items-center border-2 rounded btn-shadow">
+                        <b className="font-bold p-2 text-lg">Experience</b>
                     </div>
-
-                    <div className="col-span-1 bg-[#fafafa] h-12 w-14 flex justify-center items-center rounded">
+                    <div className="col-span-1 bg-[#fafafa] flex justify-center items-center border-2 rounded btn-shadow">
                         <Trophy size={28} />
                     </div>
                 </div>
 
-
-
-                <div className="grid grid-cols-3 rounded">
-                    <div className="col-span-1 bg-[#fafafa] h-12 w-14 flex justify-center items-center rounded">
+                <div className="grid grid-cols-3 gap-6 rounded">
+                    <div className="col-span-1 bg-[#fafafa] h-12 flex justify-center items-center border-2 rounded btn-shadow">
                         <Trophy size={28} />
                     </div>
 
-                    <div className="grid col-span-2 bg-[#fafafa] rounded justify-center items-center">
-                        3X Hackathons Wins
+                    <div className="grid col-span-2 bg-[#fafafa] justify-center items-center border-2 rounded btn-shadow">
+                        <b className="font-bold p-2 text-xl">Projects</b>
                     </div>
                 </div>
+
 
 
             </div>
